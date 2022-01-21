e1781bc637764581381f0c3d20a4bbfbfc4e1bfa
Hunterand <andersonaracati10@gmail.com>
2022-01-21 20:46:34 -0300

    nest form

--- src/pages/Form1/index.tsx
+++ src/pages/Form1/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {useForm, FormActions} from '../../contexts/FormContext'
 import Theme from '../../components/Theme';
@@ -12,10 +12,17 @@ export const Form1 = () => {
       navigate('/form2');
   }
 
+  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: FormActions.setName,
+      payload: e.target.value
+    })
+  }
+
   return (
     <Theme>
       <C.Container>
-        <p>Passo 1/3</p>
+        <p>Passo 1/3 {state.name}</p>
         <h1>Vamos começar com seu nome</h1>
         <p>Preencha o campo cabaixo com seu nome completo.</p>
 
@@ -23,7 +30,10 @@ export const Form1 = () => {
 
         <label>
           Seu nome completo
-          <input type="text" autoFocus/>
+          <input type="text"
+                  autoFocus
+                  value = {state.name}
+                  onChange = {handleNameChange}/>
         </label>
 
         <button onClick={handleNextForm}>Próximo</button>
