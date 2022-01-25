a024c681108d1c242ecc70139f1b064063fdc2cb
Hunterand <andersonaracati10@gmail.com>
2022-01-25 17:18:02 -0300

    From 2 completo

--- src/pages/Form2/index.tsx
+++ src/pages/Form2/index.tsx
@@ -1,5 +1,5 @@
-import { ChangeEvent, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { useForm, FormActions } from '../../contexts/FormContext'
 import Theme from '../../components/Theme';
 import * as C from './styles';
@@ -10,10 +10,14 @@ export const Form2 = () => {
   const { state, dispatch } = useForm();
 
   useEffect(() => {
-    dispatch({
-      type: FormActions.setCurrentStep,
-      payload: 2
-    })
+    if (state.name === '') {
+      navigate('/')
+    } else {
+      dispatch({
+        type: FormActions.setCurrentStep,
+        payload: 2
+      });
+    }
   }, []);
 
   const handleNextForm = () => {
@@ -21,12 +25,17 @@ export const Form2 = () => {
       navigate('/form3');
     } else alert("Preencha os dados.");
   }
-
+  const setLevel = (level: number) => {
+    dispatch({
+      type: FormActions.setLevel,
+      payload: level
+    })
+  }
   return (
     <Theme>
       <C.Container>
         <p>Passo 2/3 </p>
-        <h1>Vamos começar com seu nome</h1>
+        <h1>{state.name}, O que melhor descreve você?</h1>
         <p>Preencha o campo cabaixo com seu nome completo.</p>
 
         <hr />
@@ -34,12 +43,18 @@ export const Form2 = () => {
         <SelectOption
           title="Sou iniciante"
           description="Comecei a programar há menos de 2 anos"
+          icon="😀"
+          selected={state.level === 0}
+          onClick={() => setLevel(0)}
         />
         <SelectOption
           title="Sou Programador"
           description="Comecei a programar há 2 anos ou mais"
+          icon="😁"
+          selected={state.level === 1}
+          onClick={() => setLevel(1)}
         />
-
+        <Link className="backButton" to = "/">Voltar</Link>
         <button onClick={handleNextForm}>Próximo</button>
       </C.Container>
     </Theme>
--- src/pages/Form2/styles.ts
+++ src/pages/Form2/styles.ts
@@ -47,4 +47,10 @@ export const Container = styled.div`
       cursor: pointer;
       margin-top: 30px;
     }
+    .backButton {
+      font-size: 16px;
+      text-decoration: nome;
+      padding: 20px 40px;
+      color: #B8B8D4;
+    }
 `;
