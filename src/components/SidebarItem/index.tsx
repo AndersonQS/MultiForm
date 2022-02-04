adbfad5afe69eae7ef170f4bc0c1b6a686bceddd
Hunterand <andersonaracati10@gmail.com>
2022-02-04 16:31:11 -0300

    completmento

--- src/components/SidebarItem/index.tsx
+++ src/components/SidebarItem/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {ReactComponent as  ProfileIcon} from '../../svgs/profile.svg'
 
 import * as C  from './styles';
 
@@ -13,8 +14,14 @@ export const SidebarItem = ({title, description, icon,path}: Props) => {
   return (
     <C.Container>
       <Link to = {path}>
-        <h1></h1>
-        
+        <C.Info>
+          <C.Title>{title}</C.Title>
+          <C.Description>{description}</C.Description>
+        </C.Info>
+        <C.IconArea>
+          <ProfileIcon fill = "white" width = {24} height = {24}/>
+        </C.IconArea>
+        <C.Point></C.Point>
       </Link>
     </C.Container>
   );
