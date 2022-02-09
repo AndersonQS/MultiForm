bc971ba3c373b45d84bdbf26d3248365983d0e3c
Hunterand <andersonaracati10@gmail.com>
2022-02-09 11:45:01 -0300

    .

--- src/components/theme/index.tsx
+++ src/components/theme/index.tsx
@@ -1,14 +1,16 @@
-import React, { ReactNode } from 'react';
+import { ReactNode } from 'react';
 import Header from '../Header';
 import * as C from './styles';
 import { SidebarItem } from '../SidebarItem'
-import {useForm} from '../../contexts/FormContext'
+import { useForm } from '../../contexts/FormContext'
+
 type Props = {
   children: ReactNode;
 }
 
 const Theme = ({ children }: Props) => {
-  const {state} = useForm();
+  const { state } = useForm();
+
   return (
     <C.Container>
       <C.Area>
@@ -20,21 +22,21 @@ const Theme = ({ children }: Props) => {
               description="Se identifique"
               icon="profile"
               path="/"
-              active ={state.currentStep === 1}
+              active={state.currentStep === 1}
             />
             <SidebarItem
               title="Profissional"
               description="Seu nível"
               icon="book"
               path="/form2"
-              active ={state.currentStep === 2}
+              active={state.currentStep === 2}
             />
             <SidebarItem
               title="Contatos"
               description="Como te achar"
-              icon="email"
+              icon="mail"
               path="/form3"
-              active ={state.currentStep === 3}
+              active={state.currentStep === 3}
             />
           </C.Sidebar>
           <C.Page>
